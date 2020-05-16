79ca27fac708e1f441004dbd466e051e0ad6c9ed
Mauricio Viera Sotilo <mauricio.viera6@gmail.com>
2020-05-16 13:44:29 +0200

    navigation improvements

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const Navigation = () => (
-  <nav>
-    <ul>
+  <nav className="nav-menu">
+    <h1>Crypfo</h1>
+    <div class="search-box">
+      <FontAwesomeIcon icon={faSearch} />
+      <input type="text" class="search-bar" placeholder="Search crypto..." />
+      <div id="close-icon-search" class="close-icon-search">
+        <FontAwesomeIcon icon={faTimes} />
+      </div>
+    </div>
+    <div class="ham-menu">
+      <div class="center-line"></div>
+    </div>
+
+    <ul className="nav-menu">
       <li>
         <NavLink to="/">Home</NavLink>
       </li>
